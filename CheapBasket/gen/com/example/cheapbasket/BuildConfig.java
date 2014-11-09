9ec59be4f5ede48758e4b1e0aac60670f8a3bb00
philok93 <philokypro_s@hotmail.com>
2014-11-09 12:27:01 +0200

    reloaded

--- CheapBasket/gen/com/example/cheapbasket/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.cheapbasket;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- CheapBasket/gen/cy/cs361/cheapbasket/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package cy.cs361.cheapbasket;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
