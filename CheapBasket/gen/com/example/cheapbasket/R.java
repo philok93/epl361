410b46d91c8616b14c99ee4bae60e2e9973e2f67
Xarhs <cchara38@cs.ucy.ac.cy>
2014-11-11 22:25:48 +0200

    Search product funtion
    The search for product function  is not integerated with other parts

--- CheapBasket/gen/com/example/cheapbasket/R.java
+++ CheapBasket/gen/com/example/cheapbasket/R.java
@@ -20,60 +20,83 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int margin=0x7f040002;
     }
     public static final class drawable {
         public static final int barcode2=0x7f020000;
         public static final int basket=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int settings=0x7f020003;
-        public static final int tiroke=0x7f020004;
+        public static final int pork2=0x7f020003;
+        public static final int settings=0x7f020004;
+        public static final int tiroke=0x7f020005;
     }
     public static final class id {
-        public static final int TextView01=0x7f080006;
-        public static final int action_settings1=0x7f08001b;
-        public static final int bBasket=0x7f080010;
-        public static final int bScan=0x7f08000c;
-        public static final int bSearch=0x7f08000e;
-        public static final int bSettigs=0x7f080012;
-        public static final int bTakePicture=0x7f080015;
-        public static final int button1=0x7f080013;
-        public static final int editText1=0x7f08001a;
-        public static final int flContent=0x7f080000;
-        public static final int flPreview=0x7f080014;
-        public static final int imageView1=0x7f08000b;
-        public static final int imageView2=0x7f08000d;
-        public static final int imageView3=0x7f08000f;
-        public static final int imageView4=0x7f080011;
-        public static final int switch1=0x7f080016;
-        public static final int switch2=0x7f080017;
-        public static final int switch3=0x7f080018;
-        public static final int tableRow1=0x7f080002;
-        public static final int tableRow2=0x7f080003;
-        public static final int tableRow3=0x7f080004;
-        public static final int tableRow4=0x7f080005;
-        public static final int tableRow5=0x7f080007;
-        public static final int tableRow6=0x7f080008;
-        public static final int tableRow7=0x7f080009;
-        public static final int tableRow8=0x7f08000a;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080019;
+        public static final int TextView01=0x7f090011;
+        public static final int action_settings=0x7f090024;
+        public static final int action_settings1=0x7f090025;
+        public static final int bBasket=0x7f09001a;
+        public static final int bScan=0x7f090016;
+        public static final int bSearch=0x7f090018;
+        public static final int bSettigs=0x7f09001c;
+        public static final int bTakePicture=0x7f09001f;
+        public static final int button1=0x7f09001d;
+        public static final int button2=0x7f090010;
+        public static final int editText1=0x7f090023;
+        public static final int flContent=0x7f090000;
+        public static final int flPreview=0x7f09001e;
+        public static final int imageView1=0x7f09000d;
+        public static final int imageView2=0x7f090017;
+        public static final int imageView3=0x7f090019;
+        public static final int imageView4=0x7f09001b;
+        public static final int linearLayout1=0x7f09000f;
+        public static final int switch1=0x7f090020;
+        public static final int switch2=0x7f090021;
+        public static final int switch3=0x7f090022;
+        public static final int tableRow1=0x7f090001;
+        public static final int tableRow2=0x7f090004;
+        public static final int tableRow3=0x7f09000a;
+        public static final int tableRow4=0x7f090007;
+        public static final int tableRow5=0x7f090012;
+        public static final int tableRow6=0x7f090013;
+        public static final int tableRow7=0x7f090014;
+        public static final int tableRow8=0x7f090015;
+        public static final int textView1=0x7f090008;
+        public static final int textView2=0x7f090002;
+        public static final int textView3=0x7f090003;
+        public static final int textView4=0x7f090009;
+        public static final int textView5=0x7f09000c;
+        public static final int textView6=0x7f09000b;
+        public static final int textView7=0x7f090005;
+        public static final int textView8=0x7f090006;
+        public static final int textView9=0x7f09000e;
+    }
+    public static final class integer {
+        public static final int line=0x7f050000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int basket=0x7f030001;
-        public static final int navigation=0x7f030002;
-        public static final int product=0x7f030003;
-        public static final int scan=0x7f030004;
-        public static final int search=0x7f030005;
-        public static final int settings=0x7f030006;
+        public static final int activity_results=0x7f030001;
+        public static final int activity_search_product=0x7f030002;
+        public static final int basket=0x7f030003;
+        public static final int navigation=0x7f030004;
+        public static final int product=0x7f030005;
+        public static final int scan=0x7f030006;
+        public static final int search=0x7f030007;
+        public static final int settings=0x7f030008;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int hellohh=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int results=0x7f080002;
+        public static final int search_product=0x7f080003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_hellohh=0x7f060004;
+        public static final int title_activity_results=0x7f060005;
+        public static final int title_activity_search_product=0x7f060003;
     }
     public static final class style {
         /** 
@@ -97,10 +120,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
